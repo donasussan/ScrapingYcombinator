a63aa2772ec68b9318a98e9312f4d998018c9d09
Dona Sussan <88885027+donasussan@users.noreply.github.com>
2023-07-13 17:47:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
+soup = BeautifulSoup(yc_web_page, 'html.parser')
+
+article_titles = []
+article_links = []
+for article_tag in soup.find_all(name="span", class_="titleline"):
+    article_titles.append(article_tag.getText())
+    article_links.append(article_tag.find("a")["href"])
+
+article_upvotes = []
+for article in soup.find_all(name="td", class_="subtext"):
+    if article.span.find(class_="score") is None:
+        article_upvotes.append(0)
+    else:
+        article_upvotes.append(int(article.span.find(class_="score").contents[0].split()[0]))
+
+sorted_indices = sorted(range(len(article_upvotes)), key=lambda i: article_upvotes[i], reverse=True)
+print(sorted_indices)
+
+for index in sorted_indices:
+    print(article_titles[index])
+    print(article_links[index])
+    print(article_upvotes[index])
+
+    print()
